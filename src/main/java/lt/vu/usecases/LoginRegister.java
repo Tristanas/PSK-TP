42cd9663e64441a3d30e4ee940c0dc88855e7904
Tristanas <43069390+Tristanas@users.noreply.github.com>
2020-04-26 21:43:29 +0300

    Changed the first page to be a login/register page.

---
+++ src/main/java/lt/vu/usecases/LoginRegister.java
@@ -0,0 +1,44 @@
+package lt.vu.usecases;
+
+import lombok.Getter;
+import lombok.Setter;
+import lt.vu.entities.Trainer;
+import lt.vu.interceptors.LoggedInvocation;
+import lt.vu.persistence.TrainersDAO;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+@Model
+public class LoginRegister {
+    @Inject
+    private TrainersDAO trainersDAO;
+
+    private Trainer trainer;
+
+    @Setter
+    @Getter
+    private String username;
+
+    public String login() {
+        trainer = trainersDAO.findByUsername(username);
+        // No trainer found, there should be a notification for failed login.
+        if (trainer == null) {
+            return "index";
+        }
+
+        return "trainer?faces-redirect=true&teamId=" + this.trainer.getId();
+    }
+
+    @Transactional
+    @LoggedInvocation
+    public String registerTrainer() {
+        trainer = new Trainer();
+        trainer.setUsername(username);
+        trainer.setLevel(1);
+        trainer.setXp(0);
+        trainersDAO.persist(trainer);
+        return "trainer?faces-redirect=true&teamId=" + this.trainer.getId();
+    }
+}
